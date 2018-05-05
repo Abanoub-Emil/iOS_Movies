68585efaf7c0c70db243b241b8be96c016a75abb
Abanoub-Emil <Champion@bakhoums-macbook-pro.local>
2018-05-05 21:54:02 +0200

    movie class

---
+++ iOS_Movies/Movie.h
@@ -0,0 +1,13 @@
+//
+//  Movie.h
+//  iOS_Movies
+//
+//  Created by Champion on 5/5/18.
+//  Copyright © 2018 ITI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Movie : NSObject
+
+@end
---
+++ iOS_Movies/Movie.m
@@ -0,0 +1,13 @@
+//
+//  Movie.m
+//  iOS_Movies
+//
+//  Created by Champion on 5/5/18.
+//  Copyright © 2018 ITI. All rights reserved.
+//
+
+#import "Movie.h"
+
+@implementation Movie
+
+@end
